3d3360f8aad23dd7821633848dd9f8bb04ec827f
Howard <changhao@xiyou3g.com>
2016-11-03 20:47:30 +0800

    Initial Commit

---
+++ youniLib/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  youniLib
+//
+//  Created by 常昊 on 16/11/3.
+//  Copyright © 2016年 常昊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ youniLibTests/youniLibTests.m
@@ -0,0 +1,39 @@
+//
+//  youniLibTests.m
+//  youniLibTests
+//
+//  Created by 常昊 on 16/11/3.
+//  Copyright © 2016年 常昊. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface youniLibTests : XCTestCase
+
+@end
+
+@implementation youniLibTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
